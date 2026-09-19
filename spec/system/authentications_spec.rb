f3ba247007855b2c3c4a3e9621f9ff571650b8b9
riva773 <teslaicaer@gmail.com>
2026-09-19 23:45:58 +0900

    ウェルカムページの作成

--- spec/system/authentications_spec.rb
+++ spec/system/authentications_spec.rb
@@ -9,8 +9,22 @@ RSpec.describe "Authentications", type: :system do
     fill_in "パスワード", with: user.password
     click_button "ログイン"
 
-    expect(page).to have_current_path(root_path)
+    expect(page).to have_current_path(apps_path)
     expect(page).to have_content("アプリ選択")
     expect(page).to have_content("ログアウト")
+  end
+
+  it "未登録ユーザーが会員登録後にアプリ一覧へ移動できる" do
+    visit root_path
+    click_link "会員登録"
+
+    fill_in "ユーザー名", with: "新規ユーザー"
+    fill_in "メールアドレス", with: "new-user-#{SecureRandom.hex(8)}@example.com"
+    fill_in "パスワード", with: "password"
+    fill_in "user_password_confirmation", with: "password"
+    click_button "会員登録する"
+
+    expect(page).to have_current_path(apps_path)
+    expect(page).to have_content("アプリ選択")
   end
 end
