83a79eb55e8b28bc8db857a0dec65f27ec727034
riva773 <teslaicaer@gmail.com>
2026-09-21 14:57:34 +0900

    ゲストログイン機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     post "users",
       to: "devise/registrations#create",
       as: :user_registration
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
   end
   root "welcome#index"
   resources :apps, except: [ :new ]
@@ -16,4 +17,5 @@ Rails.application.routes.draw do
   resources :reviews, only: [ :create, :update ]
   get "ad_tests/results", to: "ad_tests#results"
   get "up" => "rails/health#show", as: :rails_health_check
+
 end
